5c93e44f774139713c7f5c53943f9b46d66bf921
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-12-04 17:29:01 -0500

    clear rows when full

--- tetris/blockManager.js
+++ tetris/blockManager.js
@@ -1,6 +1,7 @@
 export class BlockManager {
     constructor() {
-        this.grid = new Array(20).fill(null).map(() => new Array(10).fill(null));
+        this.grid = [];
+        this._addEmptyRows(20);
     }
 
     addBlocks(blocks) {
@@ -20,6 +21,27 @@ export class BlockManager {
     }
 
     clearCompleteRows() {
-        return 0;
+        this.grid = this.grid.filter(row => row.some(block => block === null));
+        let countRemoved = 20 - this.grid.length;
+        if (countRemoved > 0) {
+            this._addEmptyRows(countRemoved);
+            this._setNewBlockLocations();
+        }
+        return countRemoved;
+    }
+
+    _setNewBlockLocations() {
+        this.grid.forEach((row, y) => {
+            row.forEach(block => {
+                if (block !== null) {
+                    block.y = y;
+                }
+            });
+        });
+    }
+
+    _addEmptyRows(count) {
+        let rowsToAdd = new Array(count).fill(null).map(() => new Array(10).fill(null));
+        this.grid = rowsToAdd.concat(this.grid);
     }
 }
